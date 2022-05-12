077a0cec4f6837c7e0da90fbecf405777d4747f6
Eugenia <48625552+deveuge@users.noreply.github.com>
2022-05-12 18:10:57 +0200

    Uso de fichero de configuración externo para la conexión con base de datos.

--- src/Conexion.php
+++ src/Conexion.php
@@ -3,15 +3,12 @@ namespace Clases;
 use PDO;
 
 class Conexion {
-    private static $host = "localhost";
-    private static $db = "bibliotech_daw";
-    private static $user = "adminBD";
-    private static $pass = "passwordBD";
     private $conexion;
 
     public function __construct(){
+        $config = parse_ini_file("../bibliotech.ini", true);
         // Establecer conexión con base de datos
-        $this->conexion=new PDO("mysql:host=" . self::$host . ";dbname=" . self::$db, self::$user, self::$pass);
+        $this->conexion=new PDO("mysql:host=" . $config['database']['host'] . ";dbname=" . $config['database']['db'], $config['database']['user'], $config['database']['pass']);
         // Permitir capturar las excepciones
         $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
