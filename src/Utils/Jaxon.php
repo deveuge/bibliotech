a05cfea1f801283546eb65d74d6c0e91a6b2176e
Eugenia <48625552+deveuge@users.noreply.github.com>
2022-05-14 13:09:03 +0200

    Fix Heroku - Jaxon. Añadir explícitamente la request URI

--- src/Utils/Jaxon.php
+++ src/Utils/Jaxon.php
@@ -1,6 +1,9 @@
 <?php
+    $url =  "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+    $escaped_url = htmlspecialchars( $url, ENT_QUOTES, 'UTF-8' );
+
     $jaxon = jaxon();
     $jaxon->setOption('js.app.minify', TRUE);
     $jaxon->setOption('js.lib.uri', '../vendor/jaxon-php/jaxon-js/dist');
-    $jaxon->setOption('core.request.uri', (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
+    $jaxon->setOption('core.request.uri', $escaped_url);
 ?>
